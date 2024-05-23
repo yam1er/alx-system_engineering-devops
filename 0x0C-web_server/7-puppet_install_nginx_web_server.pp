7ec60b479fc251532f947b5e82767ddc3b9bb3a1
yam1er <mantynouyacoubou@gmail.com>
2024-05-23 04:20:31 -0700

    Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,19 +1,36 @@
-# installs and configs nginx server
-
+# Install Nginx package
 package { 'nginx':
-  ensure => 'installed',
+  ensure => installed,
 }
 
-file { 'index.nginx-debian.html':
-  path    => '/var/www/html/index.nginx-debian.html',
-  content => 'Hello World!',
+# Configure Nginx server
+file { '/etc/nginx/sites-available/default':
+  ensure  => present,
+  content => "
+    server {
+      listen 80;
+      root /var/www/html;
+      index index.html;
+
+      location / {
+        return 200 'Hello World!';
+      }
+
+      location /redirect_me {
+        return 301 https://www.google.com/;
+      }
+
+      error_page 404 /404.html;
+      location = /404.html {
+        return 200 'Ceci n\'est pas une page';
+      }
+    }
+  ",
 }
 
-exec { 'config':
-  command  => 'sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/www.youtube.com/watch?v=TfgBHC5gvTI permanent;/" /etc/nginx/sites-available/default',
-  provider => 'shell',
-}
-exec { 'start':
-  command  => 'sudo service nginx restart',
-  provider => 'shell',
+# Enable Nginx service
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  subscribe => File['/etc/nginx/sites-available/default'],
 }
