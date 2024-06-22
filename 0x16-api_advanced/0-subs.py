c084f8048e283a940010d219c23505bbe20c1509
yam1er <mantynouyacoubou@gmail.com>
2024-06-21 18:10:09 -0700

    Top Ten

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "MyRedditApp/1.0 (by u/yampremier)"
+        "User-Agent": "0x16-api_advanced:project:\ v1.0.0 (by /u/yampremier)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
