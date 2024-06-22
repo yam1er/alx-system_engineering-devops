c412d4cee627dee2221371e54b40e1a317c831e0
yam1er <mantynouyacoubou@gmail.com>
2024-06-21 18:12:03 -0700

    fix

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,9 +6,6 @@ import requests
 def top_ten(subreddit):
     """Print the titles of the 10 hottest posts on a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": ""
-    }
     params = {
         "limit": 10
     }
